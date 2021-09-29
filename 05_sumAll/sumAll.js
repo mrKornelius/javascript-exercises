861df1f4cfe0541a5122c28186f2ed1d6334dba3
mrKornelius <magnus.korneliusson@gmail.com>
2021-09-29 21:32:41 +0200

    JS05 Done.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,7 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    if (a < 0 || b < 0 || typeof a != 'number' || typeof b != 'number') 
+        return 'ERROR';
+    return (Math.abs(b-a)+1) * (b+a) / 2;
 };
 
 // Do not edit below this line
