f03178481da0206d3e35ec1268065cd37126ce7c
mrKornelius <magnus.korneliusson@gmail.com>
2021-09-29 23:32:05 +0200

    JS09 Done.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    
+    // remove punctation and spaces
+    ['.', ',', ' ', '!'].forEach(c => {
+        str = str.replaceAll(c, '');
+    });
 
+    return (str.split('').reverse().join('') == str)
 };
 
 // Do not edit below this line
