cac60f32bc17303163894b15280e0719ef54e9b5
mrKornelius <magnus.korneliusson@gmail.com>
2021-09-29 21:08:18 +0200

    JS04 Done.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    let result = [];
+    
+    arr.forEach(elm => {
+        if ( ! args.includes(elm))
+            result.push(elm);
+    });
+    return result;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
