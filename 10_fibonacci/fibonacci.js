a684d885fe83447c9b660306028c100adb109866
mrKornelius <magnus.korneliusson@gmail.com>
2021-10-01 00:26:10 +0200

    JS10 Done.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    if (n < 1) return 'OOPS';
+    let prev_2 = 1;
+    let prev_1 = 1;
+    let fib = 1;
+    for (let i = 2; i < n; i++) {
+        fib = prev_1 + prev_2;
+        prev_2 = prev_1;
+        prev_1 = fib;
+    }
 
+    return fib;
 };
 
 // Do not edit below this line
