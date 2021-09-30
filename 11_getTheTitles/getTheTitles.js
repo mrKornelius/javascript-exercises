0e92161ea0c6a20d279f4f28d46d75aea48f04c7
mrKornelius <magnus.korneliusson@gmail.com>
2021-10-01 00:38:07 +0200

    JS11 Done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,15 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    // let titles = [];
 
+    // books.forEach(book => {
+    //     titles.push(book['title']);
+    // });
+
+    // for (let book of books) {
+    //     titles.push(book.title);
+    // }
+
+    return books.map(book => book.title);
 };
 
 // Do not edit below this line
