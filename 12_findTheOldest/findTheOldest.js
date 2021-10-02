bb47bbe110f78bda02bd1623b3f65cb86d7182fe
mrKornelius <magnus.korneliusson@gmail.com>
2021-10-02 12:47:10 +0200

    JS12 Done.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    return people.reduce((oldest, currentPerson) => {
+        if (undefDeath(currentPerson) - currentPerson.yearOfBirth 
+            > undefDeath(oldest) - oldest.yearOfBirth)
+            oldest = currentPerson;
+        return oldest;
+    });
+};
 
+const undefDeath = function undefinedYearOfDeath(obj) {
+    return obj.yearOfDeath === undefined 
+        ? new Date().getFullYear() 
+        : obj.yearOfDeath;
 };
 
 // Do not edit below this line
